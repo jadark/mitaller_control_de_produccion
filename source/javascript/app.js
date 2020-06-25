95d56761dd285cd6966aae9a26b38c7668d57eca
Javier Castro <javiercastro.ik@gmail.com>
2020-06-25 02:01:11 -0500

    fix styles by email

--- source/javascript/app.js
+++ source/javascript/app.js
@@ -48,23 +48,24 @@ if (chartProgress) {
 
   });
 }
-document.querySelector(".line_etapa").addEventListener('click', function (event) {
-  event.preventDefault();
-  var container = document.querySelector(".box_container .box_inner");
-    if (!container.classList.contains('active')) {
-      container.classList.add('active');
-      container.style.height = 'auto';
-      var height = container.clientHeight + 'px';
-      container.style.height = '0px';
-      setTimeout(function () {
-        container.style.height = height;
-      }, 0);
-    } else {
-      container.style.height = '0px';
-      container.addEventListener('transitionend', function () {
-        container.classList.remove('active');
-      }, {
-        once: true
-      });
-    }
-})
+// Dropdrown Select
+// document.querySelector(".line_etapa").addEventListener('click', function (event) {
+//   event.preventDefault();
+//   var container = document.querySelector(".box_container .box_inner");
+//     if (!container.classList.contains('active')) {
+//       container.classList.add('active');
+//       container.style.height = 'auto';
+//       var height = container.clientHeight + 'px';
+//       container.style.height = '0px';
+//       setTimeout(function () {
+//         container.style.height = height;
+//       }, 0);
+//     } else {
+//       container.style.height = '0px';
+//       container.addEventListener('transitionend', function () {
+//         container.classList.remove('active');
+//       }, {
+//         once: true
+//       });
+//     }
+// })
